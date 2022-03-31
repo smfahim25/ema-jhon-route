d51250040d600a01a42f613eea984ebe7f0ea64d
smfahiim25 <smfahimhossen@gmail.com>
2022-04-01 00:55:24 +0600

    code  changed

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,54 @@
+
+import { Link } from 'react-router-dom';
+import { addToDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import useCart from '../hooks/useCart';
+import useProducts from '../hooks/useProducts';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+
+    const handleAddToCart = (selectedProduct) => {
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
+        setCart(newCart);
+        addToDb(selectedProduct.id);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <Link to="/orders">
+                        <button className='review-order'>Review Order</button>
+                    </Link>
+                </Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
